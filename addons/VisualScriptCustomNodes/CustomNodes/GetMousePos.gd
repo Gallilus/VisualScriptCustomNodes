caba6d8169dd2e923f526a23b63dbe7c3cb18fd6
David Cambré <david.cambre@gmail.com>
2021-08-01 17:55:43 +0200

    Create GetMousePos.gd

---
+++ addons/VisualScriptCustomNodes/CustomNodes/GetMousePos.gd
@@ -0,0 +1,74 @@
+tool
+extends VisualScriptCustomNode
+
+export var sequenced := false setget set_sequenced
+func set_sequenced(value):
+	sequenced = value
+	ports_changed_notify()
+func _has_input_sequence_port():
+	return sequenced
+func _get_output_sequence_port_count():
+	return 0 + int(sequenced)
+
+enum _relatives {global, local, viewport}
+export (_relatives) var relative := 0 setget set_relative
+func set_relative(value):
+	if not value == relative:
+		relative = value
+		ports_changed_notify()
+
+
+func _get_category():
+	return "data"
+
+func _get_caption():
+	return "Get mouse pos"
+
+func _get_text():
+	return "text"
+
+
+func _get_input_value_port_count():
+	return 1
+
+func _get_input_value_port_name(idx):
+	return "self"
+
+func _get_input_value_port_type(idx):
+	TYPE_OBJECT
+
+
+func _get_output_value_port_count():
+	return 1
+
+func _get_output_value_port_name(idx):
+	match relative:
+		0:
+			return "g_pos"
+		1:
+			return "l_pos"
+		2:
+			return "vp_pos"
+
+func _get_output_value_port_type(idx):
+	return TYPE_VECTOR2
+
+
+func _step(inputs, outputs, start_mode, working_mem):
+	if !inputs[0]:
+		return "Input[0] not connected to self"
+	
+	match relative:
+		0:
+			if inputs[0].has_method("get_global_mouse_position"):
+				outputs[0] = inputs[0].get_global_mouse_position()
+		1:
+			if inputs[0].has_method("get_local_mouse_position"):
+				outputs[0] = inputs[0].get_local_mouse_position()
+		2:
+			if inputs[0].has_method("get_mouse_position"):
+				outputs[0] = inputs[0].get_mouse_position()
+			if inputs[0].has_method("get_viewport"):
+				outputs[0] = inputs[0].get_viewport().get_mouse_position()
+	
+	return 0
