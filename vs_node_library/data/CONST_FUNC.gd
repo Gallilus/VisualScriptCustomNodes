afdbc521e303c9ac622c61239ccd1514cf9b6e61
David Cambré <david.cambre@gmail.com>
2021-08-27 21:28:39 +0200

    Update CONST_FUNC.gd

--- vs_node_library/data/CONST_FUNC.gd
+++ vs_node_library/data/CONST_FUNC.gd
@@ -5,6 +5,6 @@ func _constant_function():
 #	return "string"
 #	return Input.MOUSE_MODE_CAPTURED
 #	return Input.get_axis("ui_up","ui_down")
-	var r = Panel.new()
+	var r = Button.new()
 	r.rect_size = Vector2.ONE * 64
 	return r
